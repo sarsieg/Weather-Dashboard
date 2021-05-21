edfbd517d18eeb69ce0ead466cff8ff1ad1b4f8b
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-05-20 21:41:46 -0500

    created elements for forecast month and year as well as a for loop to to display forecast for the next 5 days underneath current conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,7 +43,38 @@ function initPage() {
                 UVIndex.innerHTML = response.data[0].value;
                 currentUVEl.innerHTML = "UV Index: ";
                 currentUVEl.append(UVIndex);
+            });
+
+            let cityID = response.data.id;
+            let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
+            axios.get(forecastQueryURL)
+            .then(function(response){
+                console.log(response);
+                const forecastEls = document.querySelectorAll(".forecast");
+                for (i=0; i<forecastEls.length; i++) {
+                    forecastEls[i].innerHTML = "";
+                    const forecastIndex = i*8 + 4;
+                    const forecastDate = new Date(response.data.list[forecastIndex].dt * 1000);
+                    const forecastDay = forecastDate.getDate();
+                    const forecastMonth = forecastDate.getMonth() + 1;
+                    const forecastYear = forecastDate.getFullYear();
+
+
+                    const forecastDateEl = document.createElement("p");
+                    forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
+                    forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+                    forecastEls.append(forecastDateEl);
+
+                    const forecastWeatherEl = document.createElement("img");
+                    forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
+                    forecastWeatherEl.setAttribute("alt",response.data.list[forecastIndex].main.temp) + " &#176F";
+                    forecastEls[i].append(forecastWeatherEl);
+
+                    
+
+                }
             })
+
 
         })
     }
